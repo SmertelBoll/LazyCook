f5df334e879c01d4d350579722c57d6f364ddacc
Lyubomyr <sholop2113@gmail.com>
2023-02-15 00:39:31 +0200

    responsive typography

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -2,10 +2,12 @@ import { createTheme } from "@mui/material/styles";
 import palette from './palette.js'
 import spacing from './spacing.js'
 import shadows from './shadows.js'
+import typography from './typography.js'
 
 
 export const theme = createTheme({
   palette,
   spacing,
   shadows,
+  typography,
 })
---
+++ src/theme/typography.js
@@ -0,0 +1,58 @@
+const MAXWIDTH = 1920
+const MINWIDTH = 320
+
+
+const largeFormula = (minSize, maxSize) => {
+  return `calc(${minSize}px + ${maxSize-minSize} * (100vw / ${MAXWIDTH}))`
+}
+const smallFormula = (minSize, maxSize) => {
+  return `calc(${minSize}px + (${maxSize-minSize} + ${maxSize-minSize} * 0.7) * ((100vw - ${MINWIDTH}px) / ${MAXWIDTH}))`
+}
+
+const typography = {
+  fontFamily: "'Kanit', sans-serif",
+  fontWeightRegular: 400,
+  fontWeightMedium: 600,
+  fontWeightBold: 700,
+  h1: {
+    fontSize: smallFormula(48, 96),
+    '@media (min-width:767px)': {
+      fontSize: largeFormula(48, 96),
+    },
+    fontWeight: 600,
+  },
+  h2: {
+    fontSize: smallFormula(40, 80),
+    '@media (min-width:767px)': {
+      fontSize: largeFormula(40, 80),
+    },
+    fontWeight: 600,
+  },
+  h3: {                                  // header button, ingredient text
+    fontSize: smallFormula(16, 28),
+    '@media (min-width:767px)': {
+      fontSize: largeFormula(16, 28),
+    },
+    fontWeight: 400,
+    textTransform: "unset",
+  },
+  p: {
+    fontSize: smallFormula(16, 32),
+    '@media (min-width:767px)': {
+      fontSize: largeFormula(16, 32),
+    },
+    fontWeight: 400,
+    textTransform: "unset",
+  },
+  logo: {
+    fontSize: smallFormula(24, 44),
+    '@media (min-width:767px)': {
+      fontSize: largeFormula(24, 44),
+    },
+    fontWeight: 700,
+  },
+  
+}
+
+export default typography;
+
