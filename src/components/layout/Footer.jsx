e8be30e5ea00b455b2a1e7745e012a31c706cdc7
Lyubomyr <sholop2113@gmail.com>
2023-02-15 19:43:48 +0200

    finished the Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useTheme } from "@emotion/react";
+import { Typography, Box } from "@mui/material";
+import React from "react";
+import { StyledContainer } from "../custom/customComponents";
 
 function Footer() {
+  const theme = useTheme();
+
   return (
-    <div>Footer</div>
-  )
+    <Box
+      position="static"
+      component="footer"
+      bgcolor={theme.palette.bg.main}
+      sx={{
+        py: 3,
+      }}
+    >
+      <StyledContainer>
+        <Typography
+          variant="h3"
+          sx={{ textAlign: "center", color: "text.grey" }}
+        >
+          © 2023 LazyCook
+        </Typography>
+      </StyledContainer>
+    </Box>
+  );
 }
 
-export default Footer
+export default Footer;
