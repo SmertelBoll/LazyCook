837c8588c831ddd0795a016f7a4fee52f4e39965
Lyubomyr <sholop2113@gmail.com>
2023-06-12 22:09:25 +0300

    some fixed for url

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 
 import "./styles/index.css";
@@ -9,8 +9,8 @@ import { theme } from "./theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter basename="https://smertelboll.github.io/LazyCook/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>
 );
